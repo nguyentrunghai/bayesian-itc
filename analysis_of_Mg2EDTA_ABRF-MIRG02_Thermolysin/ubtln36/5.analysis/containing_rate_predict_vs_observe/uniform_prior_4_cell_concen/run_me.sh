3d0d695a74ce8903b216e49b0cb81e87228c8483
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-04-20 13:29:49 -0500

    add new ci_vertical_stack/ and containing_rate_predict_vs_observe/

---
+++ analysis_of_Mg2EDTA_ABRF-MIRG02_Thermolysin/ubtln36/5.analysis/containing_rate_predict_vs_observe/uniform_prior_4_cell_concen/run_me.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export SCRIPT="/home/tnguye46/opt/src/bayesian-itc/analysis_of_Mg2EDTA_ABRF-MIRG02_Thermolysin/scripts/run_plot_containing_rate_of_cis.py"
+
+export MCMC_DIR="/home/tnguye46/bayesian_itc_reproduce/ubtln36/2.bitc_mcmc_uniform_prior_4_cell_concen/repeat_0"
+
+export NONLIN_RES="/home/tnguye46/bayesian_itc_reproduce/ubtln36/3.nonlinear_fit_results/origin_dg_dh_in_kcal_per_mole.dat"
+
+export EXPERIMENTS="19052014ubtln36test2 ubtln36a ubtln36a14082014 ubtln36b ubtln36b18082014 ubtln36c14082014 ubtln36d ubtln36e ubtln36f ubtln36g ubtln36h"
+
+# DeltaG
+python $SCRIPT --bitc_mcmc_dir $MCMC_DIR --nonlinear_fit_result_file $NONLIN_RES --ordered_experiment_names "$EXPERIMENTS" --parameter "DeltaG" 
+
+
+# DeltaH
+python $SCRIPT --bitc_mcmc_dir $MCMC_DIR --nonlinear_fit_result_file $NONLIN_RES --ordered_experiment_names "$EXPERIMENTS" --parameter "DeltaH"
+
+
