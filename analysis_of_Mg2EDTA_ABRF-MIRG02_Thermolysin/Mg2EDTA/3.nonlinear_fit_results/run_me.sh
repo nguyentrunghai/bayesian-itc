4db706692eaf1fe5a94149502204bc56fcc7c657
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-05-02 13:54:40 -0500

    add new Mg2EDTA/

---
+++ analysis_of_Mg2EDTA_ABRF-MIRG02_Thermolysin/Mg2EDTA/3.nonlinear_fit_results/run_me.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export SCRIPT="/home/tnguye46/opt/src/bayesian-itc/analysis_of_Mg2EDTA_ABRF-MIRG02_Thermolysin/scripts/run_convert_ka_to_dg.py"
+
+export ORIGIN_PAR_FILE="origin_ka_dh_in_cal_per_mole.dat"
+
+export ITC_DIR="/home/tnguye46/bayesian_itc/Mg2EDTA/1.itc_origin_heat_files"
+
+export ENERGY_UNIT="cal_per_mole"
+
+export OUT="origin_dg_dh_in_kcal_per_mole.dat"
+
+python $SCRIPT --origin_par_file $ORIGIN_PAR_FILE --itc_file_dir $ITC_DIR --input_energy_unit $ENERGY_UNIT --write_header --out $OUT
+
