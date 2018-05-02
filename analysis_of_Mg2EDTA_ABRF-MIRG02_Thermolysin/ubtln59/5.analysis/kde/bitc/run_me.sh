7df7b03407db7f2b258575ed518497a0a0d66a1a
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-05-02 14:44:36 -0500

    delete ubtln59/

--- analysis_of_Mg2EDTA_ABRF-MIRG02_Thermolysin/ubtln59/5.analysis/kde/bitc/run_me.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export SCRIPT="../../../../scripts/run_kde_2d.py"
-
-export MCMC_DIR="../../../2.bitc_mcmc/repeat_0"
-
-export EXPER="ubtln59a ubtln59a21082014 ubtln59altesProtein ubtln59b ubtln59b21082014 ubtln59c ubtln59c21082014 ubtln59d ubtln59neuesProtein"
-
-python $SCRIPT --mcmc_dir $MCMC_DIR --ordered_experiment_names "$EXPER"
