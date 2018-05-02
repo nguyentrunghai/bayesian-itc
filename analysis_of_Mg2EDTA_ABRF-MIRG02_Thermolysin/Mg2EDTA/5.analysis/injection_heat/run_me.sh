82f7544c5c53af1d28f145997f8f1e038fedab69
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-05-02 13:42:47 -0500

    delete Mg2EDTA/

--- analysis_of_Mg2EDTA_ABRF-MIRG02_Thermolysin/Mg2EDTA/5.analysis/injection_heat/run_me.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export SCRIPT="../../../scripts/run_injection_heat.py"
-
-export TRACE="../../2.bitc_mcmc/repeat_0/Mg1EDTAp1a/traces.pickle"
-
-python $SCRIPT --mcmc_trace_file $TRACE
-
